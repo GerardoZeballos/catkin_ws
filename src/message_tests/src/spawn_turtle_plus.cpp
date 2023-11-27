9fba14ed15ceda66abefae5b9bce27064ce2c8b8
GerardoZeballos <gerardo.zeballos@ucb.edu.bo>
2023-11-27 07:08:45 +0100

    Update12

--- src/message_tests/src/spawn_turtle_plus.cpp
+++ src/message_tests/src/spawn_turtle_plus.cpp
@@ -1,29 +1,36 @@
+//This program spawns a new turtlesim turtle by calling
+// the appropriate service.
 #include <ros/ros.h>
+//The srv class for the service.
 #include <turtlesim/Spawn.h>
-#include <geometry_msgs/Twist.h>
 #include <std_srvs/Empty.h>
+#include <geometry_msgs/Twist.h>
+#include <message_tests/Changerate.h>
+#include <message_tests/StopStart.h>
 #include <message_tests/ChangeSpeed.h>
 
-bool forward = true;
-double current_speed = 1.0;
+ros::ServiceClient toggle_forward;
+ros::ServiceClient Speed;
 ros::Publisher My_turtle;
 
-bool toggleForward(
-	std_srvs::Empty::Request &req,
-	std_srvs::Empty::Response &resp){
-        forward = !forward;
-        ROS_INFO_STREAM("Now sending "<<(forward?
-                "forward":"rotate")<< " commands.");
-	return true;
+
+void toggleForward() {
+    std_srvs::Empty empty;
+    if (toggle_forward.call(empty)) {
+        ROS_INFO("Toggled forward");
+    } else {
+        ROS_ERROR("Failed to toggle forward");
+    }
 }
 
-bool changeSpeed(
-    message_tests::ChangeSpeed::Request &req,
-    message_tests::ChangeSpeed::Response &resp){
-        current_speed = req.new_speed;
-        ROS_INFO_STREAM("Changing speed to "<<current_speed);
-        resp.success = true; 
-        return true;
+void setInitialSpeed() {
+    message_tests::ChangeSpeed change_speed;
+    change_speed.request.new_speed = 5.0;
+    if (Speed.call(change_speed)) {
+        ROS_INFO("Set initial speed to 5.0");
+    } else {
+        ROS_ERROR("Failed to set initial speed");
+    }
 }
 
 void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg) {
@@ -32,12 +39,18 @@ void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg) {
 
 int main(int argc, char **argv){
 
-    ros::init(argc, argv, "spawn_turtle_plus");
+    ros::init(argc, argv, "spawn_turtle");
     ros::NodeHandle nh;
 
-    ros::ServiceClient spawnClient
-		= nh.serviceClient<turtlesim::Spawn>("spawn");
+    ros::ServiceClient toggle_Forward= nh.serviceClient<std_srvs::Empty>("/toggle_forward");  
+    ros::ServiceClient Speed= nh.serviceClient<std_srvs::Empty>("/Change_speed");
+    toggleForward();
+    setInitialSpeed();
 
+    ros::Subscriber sub_cmd_vel = nh.subscribe("turtle1/cmd_vel", 1000, cmdVelCallback);
+    ros::Publisher My_turtle = nh.advertise<geometry_msgs::Twist>( "Myturtle/cmd_vel", 1000);
+
+    ros::ServiceClient spawnClient= nh.serviceClient<turtlesim::Spawn>("spawn");
     turtlesim::Spawn::Request req;
     turtlesim::Spawn::Response resp;
 
@@ -48,7 +61,6 @@ int main(int argc, char **argv){
 
     ros::service::waitForService("spawn", ros::Duration(5));
     bool success = spawnClient.call(req,resp);
-   
 
     if(success){
 	ROS_INFO_STREAM("Spawned a turtle named "
@@ -56,22 +68,4 @@ int main(int argc, char **argv){
     }else{
 	ROS_ERROR_STREAM("Failed to spawn.");
     }
-
-    ros::ServiceServer server = nh.advertiseService("toggle_forward",&toggleForward);
-               
-    ros::Subscriber sub= nh.subscribe("turtle1/cmd_vel", 1000, cmdVelCallback);
-    My_turtle = nh.advertise<geometry_msgs::Twist>("MyTurtle/cmd_vel", 1000);
-    
-    
-    
-    ros::Rate rate(2);
-	
-	while(ros::ok()){
-		geometry_msgs::Twist msg;
-             msg.linear.x = forward ? current_speed : 0.0;
-             msg.angular.z = forward ? 0.0 : current_speed;
-		pub.publish(msg);
-		ros::spinOnce();
-		rate.sleep();
-	}
 }
