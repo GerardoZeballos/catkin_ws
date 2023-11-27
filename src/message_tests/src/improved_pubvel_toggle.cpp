5936ecbff23a38e993a644a9d633144f552dcdfc
GerardoZeballos <gerardo.zeballos@ucb.edu.bo>
2023-11-27 04:29:36 +0100

    Update9

--- src/message_tests/src/improved_pubvel_toggle.cpp
+++ src/message_tests/src/improved_pubvel_toggle.cpp
@@ -8,6 +8,8 @@
 bool forward = true;
 double newfrequency;
 bool ratechanged = false;
+bool StopRun = true;
+double current_speed = 1.0;
 
 bool toggleForward(
 	std_srvs::Empty::Request &req,
@@ -29,6 +31,15 @@ bool changeRate(
         return true;
 }
 
+bool Startstop(
+    message_tests::ToggleTurtle::Request &req,
+    message_tests::ToggleTurtle::Response &resp){
+        StopRun = req.run_turtle;
+        ROS_INFO_STREAM("Turtle is "<<(StopRun?
+                "running":"stopped"));
+        return true;
+}
+
 
 int main(int argc, char **argv){
         ros::init(argc,argv,"pubvel_toggle_rate");
@@ -39,21 +50,29 @@ int main(int argc, char **argv){
                 
         ros::ServiceServer server0 =
                 nh.advertiseService("change_rate",&changeRate);
+        ros::ServiceServer server1 =
+                nh.advertiseService("change_rate",&Startstop);
                 
         ros::Publisher pub=nh.advertise<geometry_msgs::Twist>(
 		"turtle1/cmd_vel",1000);
     
         ros::Rate rate(2);
-	while(ros::ok()){
-		geometry_msgs::Twist msg;
-                msg.linear.x = forward?1.0:0.0;
-                msg.angular.z=forward?0.0:1.0;
-		pub.publish(msg);
-		ros::spinOnce();
-                if(ratechanged) {
-                    rate = ros::Rate(newfrequency);
-                    ratechanged = false;
-                }
-		rate.sleep();
-	}
+	 while(ros::ok()){
+        geometry_msgs::Twist msg;
+        if (Startstop) {
+            msg.linear.x = forward ? current_speed : 0.0;
+            msg.angular.z = forward ? 0.0 : current_speed;
+        } else {
+            msg.linear.x = 0.0;
+            msg.angular.z = 0.0;
+        }
+
+        pub.publish(msg);
+        ros::spinOnce();
+        if (ratechanged) {
+            rate = ros::Rate(newfrequency);
+            ratechanged = false;
+        }
+        rate.sleep();
+    }
 }
